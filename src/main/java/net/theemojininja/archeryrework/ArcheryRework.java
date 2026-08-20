f3d54904bdfec60b313fbdbc7dff9684efbc77a6
TheRealEmojiNinja <amanpatra2024@gmail.com>
2026-08-20 10:34:29 -0400

    Initial commit

---
+++ src/main/java/net/theemojininja/archeryrework/ArcheryRework.java
@@ -0,0 +1,23 @@
+package net.theemojininja.archeryrework;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArcheryRework implements ModInitializer {
+	public static final String MOD_ID = "archeryrework";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
---
+++ src/main/java/net/theemojininja/archeryrework/ArcheryReworkDataGenerator.java
@@ -0,0 +1,11 @@
+package net.theemojininja.archeryrework;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ArcheryReworkDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
